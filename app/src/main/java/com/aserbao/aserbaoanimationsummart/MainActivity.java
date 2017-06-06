fb0b4c3e6ac3140d5b4a782d152ffee89ca6ecd8
aserbao <1142803753@qq.com>
2017-06-07 00:54:28 +0800

    interpolator

--- app/src/main/java/com/aserbao/aserbaoanimationsummart/MainActivity.java
+++ app/src/main/java/com/aserbao/aserbaoanimationsummart/MainActivity.java
@@ -1,8 +1,8 @@
 package com.aserbao.aserbaoanimationsummart;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/aserbao/aserbaoanimationsummart/MoveActivity.java
+++ app/src/main/java/com/aserbao/aserbaoanimationsummart/MoveActivity.java
@@ -3,21 +3,37 @@ package com.aserbao.aserbaoanimationsummart;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
-import android.widget.FrameLayout;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MoveActivity extends AppCompatActivity {
 
+
     @BindView(R.id.move_image_view)
     ImageView mMoveImageView;
+    @BindView(R.id.translate)
+    Button mButton1;
+    @BindView(R.id.alpha)
+    Button mButton2;
+    @BindView(R.id.scale)
+    Button mScale;
+    @BindView(R.id.rotate)
+    Button mRotate;
     @BindView(R.id.frameLayout)
-    FrameLayout mFrameLayout;
+    RelativeLayout mFrameLayout;
+    private Animation mAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +42,27 @@ public class MoveActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    public void btn_move_start(View view) {
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.move_rotate);
-        /*Animation translate = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, -0.2f,
-                Animation.RELATIVE_TO_PARENT, 1, Animation.RELATIVE_TO_SELF, 0,
-                Animation.RELATIVE_TO_SELF, 0);*/
-        Animation translate = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 0.5f,
-                Animation.RELATIVE_TO_PARENT, -0.5f, Animation.RELATIVE_TO_SELF, 0,
-                Animation.RELATIVE_TO_SELF, 0);
-        translate.setDuration(3000);
-        mMoveImageView.startAnimation(translate);
+    @OnClick({R.id.translate, R.id.alpha, R.id.scale, R.id.rotate})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.translate:
+                mAnimation = new TranslateAnimation(Animation.RELATIVE_TO_PARENT,-0.5f,Animation.RELATIVE_TO_PARENT,0.5f,Animation.RELATIVE_TO_PARENT,-0.5f,Animation.RELATIVE_TO_PARENT,0.5f);
+                break;
+            case R.id.alpha:
+                mAnimation = new AlphaAnimation(1, 0);
+                break;
+            case R.id.scale:
+                mAnimation = new ScaleAnimation(1,3,1,3);
+                break;
+            case R.id.rotate:
+                mAnimation = new RotateAnimation(0,360,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+                break;
+        }
+        Interpolator interpolator = new CycleInterpolator(0.55f);
+        mAnimation.setInterpolator(interpolator);
+        mAnimation.setDuration(3000);
+        mAnimation.setFillAfter(true);
+        mAnimation.setRepeatCount(5);
+        mMoveImageView.startAnimation(mAnimation);
     }
 }
