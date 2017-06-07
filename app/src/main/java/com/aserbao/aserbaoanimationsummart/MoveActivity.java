5162411cbe4a0c3294151f73280814446c2bbd89
aserbao <1142803753@qq.com>
2017-06-07 23:37:39 +0800

    what

--- app/src/main/java/com/aserbao/aserbaoanimationsummart/MoveActivity.java
+++ app/src/main/java/com/aserbao/aserbaoanimationsummart/MoveActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
