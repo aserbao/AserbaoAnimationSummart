2964349a4e279178b2137ea9fdea462e426ca3e6
aserbao <1142803753@qq.com>
2017-06-07 23:44:06 +0800

    创建PlayFishActivity

--- app/src/main/java/com/aserbao/aserbaoanimationsummart/PlayFishActivity.java
+++ app/src/main/java/com/aserbao/aserbaoanimationsummart/PlayFishActivity.java
@@ -2,17 +2,12 @@ package com.aserbao.aserbaoanimationsummart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class SurfaceViewSummaryActivity extends AppCompatActivity {
+public class PlayFishActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_surface_view_summary);
-    }
-
-    public void btn_play_fish(View view) {
-
+        setContentView(R.layout.activity_play_fish);
     }
 }
