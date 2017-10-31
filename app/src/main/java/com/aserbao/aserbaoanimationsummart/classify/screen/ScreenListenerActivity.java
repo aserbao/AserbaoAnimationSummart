b2d9de0e7c2ce2d84663bb4b49c3afcaca805a10
aserbao <1142803753@qq.com>
2017-10-31 10:14:25 +0800

    实现屏幕监听

--- app/src/main/java/com/aserbao/aserbaoanimationsummart/classify/screen/ScreenListenerActivity.java
+++ app/src/main/java/com/aserbao/aserbaoanimationsummart/classify/screen/ScreenListenerActivity.java
@@ -1,5 +1,6 @@
 package com.aserbao.aserbaoanimationsummart.classify.screen;
 
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,34 @@ import com.aserbao.aserbaoanimationsummart.R;
 
 public class ScreenListenerActivity extends AppCompatActivity {
 
+    private Screen mScreen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_listener);
+        mScreen = new Screen();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.intent.action.SCREEN_OFF");
+        filter.addAction("android.intent.action.SCREEN_ON");
+        this.registerReceiver(mScreen,filter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mScreen);
     }
 }
