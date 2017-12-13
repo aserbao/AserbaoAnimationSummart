4f75d64c885aef922af15a9e0a6164707593e986
aserbao <1142803753@qq.com>
2017-12-13 21:51:02 +0800

    查看View的源码

--- app/src/main/java/com/aserbao/aserbaoanimationsummart/classify/module/ModuleActivity.java
+++ app/src/main/java/com/aserbao/aserbaoanimationsummart/classify/module/ModuleActivity.java
@@ -1,14 +1,14 @@
 package com.aserbao.aserbaoanimationsummart.classify.module;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 import com.aserbao.aserbaoanimationsummart.R;
 import com.aserbao.aserbaoanimationsummart.classify.module.activity.LauncherActivity;
 import com.aserbao.aserbaoanimationsummart.classify.module.fragments.FragmentActivity;
+import com.aserbao.aserbaoanimationsummart.classify.module.soundCode.SoundCodeActivity;
 import com.aserbao.aserbaoanimationsummart.classify.module.ui.UIActivity;
 
 public class ModuleActivity extends AppCompatActivity {
@@ -29,5 +29,9 @@ public class ModuleActivity extends AppCompatActivity {
 
     public void btn_ui(View view) {
         startActivity(new Intent(this, UIActivity.class));
+    }
+
+    public void btn_source_code(View view) {
+        startActivity(new Intent(this, SoundCodeActivity.class));
     }
 }
---
+++ app/src/main/java/com/aserbao/aserbaoanimationsummart/classify/module/soundCode/SoundCodeActivity.java
@@ -0,0 +1,15 @@
+package com.aserbao.aserbaoanimationsummart.classify.module.soundCode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aserbao.aserbaoanimationsummart.R;
+
+public class SoundCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_code);
+    }
+}
